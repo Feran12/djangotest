6e908d125390fec30d501c0680455cebcdff3308
Feran12 <131621901+Feran12@users.noreply.github.com>
2023-10-23 17:58:20 +0200

    Update views.py

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def home(request):
+def test(request):
     return HttpResponse('Hellow World!')
+
+@csrf_exempt
+def home(request: HttpRequest):
+    if request.method == 'POST':
+        test = request.POST.get('inputData')
+        outputtest = test + "processed"
+        return JsonResponse({'outputData' : outputtest})
